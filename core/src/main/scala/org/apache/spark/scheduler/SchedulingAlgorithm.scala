7924865487cfbc2119cc42e169c39703c0c1b2df
suyongbing <suyongbing@kingsoft.com>
2021-10-15 20:57:10 +0800

    Pool

--- core/src/main/scala/org/apache/spark/scheduler/SchedulingAlgorithm.scala
+++ core/src/main/scala/org/apache/spark/scheduler/SchedulingAlgorithm.scala
@@ -18,6 +18,12 @@
 package org.apache.spark.scheduler
 
 /**
+ * 定义了调度算法的比较规范
+ *          SchedulingAlgorithm
+ *          /                 \
+ * FIFOSchedulingAlgorithm   FairSchedulingAlgorithm
+ *    (先进先出调度算法)         (公平调度算法)
+ *
  * An interface for sort algorithm
  * FIFO: FIFO algorithm between TaskSetManagers
  * FS: FS algorithm between Pools, and FIFO or FS within Pools
@@ -28,14 +34,28 @@ private[spark] trait SchedulingAlgorithm {
 
 private[spark] class FIFOSchedulingAlgorithm extends SchedulingAlgorithm {
   override def comparator(s1: Schedulable, s2: Schedulable): Boolean = {
+    // 获取s1的权限
     val priority1 = s1.priority
+    // 获取s2的权限
     val priority2 = s2.priority
+    // 权限比较,权限值越小，其越优先被调用
+    /**
+     * math.signum(Int)
+     * 此方法返回参数的符号函数：
+     * 1）若参数为+0或者-0,返回0
+     * 2）若参数为正数则返回+1，参数为负数则返回-1
+     * 3）若参数NaN,则返回NaN
+     */
     var res = math.signum(priority1 - priority2)
+    // 当权限比较相同的时候，比较StageId
     if (res == 0) {
       val stageId1 = s1.stageId
       val stageId2 = s2.stageId
       res = math.signum(stageId1 - stageId2)
     }
+    /**
+     * 如果比较的结果小于0，则优先调度s1，否则优先调度s2
+     */
     res < 0
   }
 }
@@ -50,6 +70,7 @@ private[spark] class FairSchedulingAlgorithm extends SchedulingAlgorithm {
     val s2Needy = runningTasks2 < minShare2
     val minShareRatio1 = runningTasks1.toDouble / math.max(minShare1, 1.0)
     val minShareRatio2 = runningTasks2.toDouble / math.max(minShare2, 1.0)
+    // taskToWeightRatio是正在运行的任务数量与权重（weight）之间的比值。
     val taskToWeightRatio1 = runningTasks1.toDouble / s1.weight.toDouble
     val taskToWeightRatio2 = runningTasks2.toDouble / s2.weight.toDouble
 
