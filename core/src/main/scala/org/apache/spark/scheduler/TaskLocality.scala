b3a376a7be27add9316263b1c0c801a8f9139af7
suyongbing <suyongbing@kingsoft.com>
2021-10-27 18:14:22 +0800

    Task本地性

--- core/src/main/scala/org/apache/spark/scheduler/TaskLocality.scala
+++ core/src/main/scala/org/apache/spark/scheduler/TaskLocality.scala
@@ -23,7 +23,8 @@ import org.apache.spark.annotation.DeveloperApi
 object TaskLocality extends Enumeration {
   // Process local is expected to be used ONLY within TaskSetManager for now.
   /**
-   * PROCESS_LOCAL（本地进程）,NODE_LOCAL（本地节点）, NO_PREF（没有偏好）, RACK_LOCAL（本地机架）, ANY（任何）。
+   * 本地性级别，由高到低分别为P:
+   *    ROCESS_LOCAL（本地进程）,NODE_LOCAL（本地节点）, NO_PREF（没有偏好）, RACK_LOCAL（本地机架）, ANY（任何）。
    */
   val PROCESS_LOCAL, NODE_LOCAL, NO_PREF, RACK_LOCAL, ANY = Value
 
